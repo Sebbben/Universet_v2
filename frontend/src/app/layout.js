4a5c594b0cc9d045f9b8c9f211fda458146b0ab7
Sebbben <sebastian.jossang@gmail.com>
2024-12-30 17:13:50 +0100

    Initial docker setup with frontend

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Hello world",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
